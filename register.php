a712d3ce9678f65bc3feeb4b251cd6845f093194
deerschen <deerschen@163.com>
2018-05-22 20:30:46 +0800

    222

--- register.php
+++ register.php
@@ -1,52 +1,48 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
-	class Res {
-		public $code;
-		public $message;
-	}
+	header('Content-type: application/json');
 
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$repeatPassword = $_POST["repeatPassword"];
+	$data = [];
+	$username = isset($_POST["username"]) ? $_POST["username"] : '';
+	$password = isset($_POST["password"]) ? $_POST["password"] : '';
+	$repeatPassword = isset($_POST["repeatPassword"]) ? $_POST["repeatPassword"] : '';
 
-	$link = mysqli_connect("127.0.0.1", "root", "123456");
+	$link = mysqli_connect("127.0.0.1", "root", "123456", "web");
+
 	if($link) {
-		$select = mysqli_select_db($link, "user");
-		echo $select;
+		$select = mysqli_select_db($link, "web");
 		if($select) {
 			if($username=="" || $password==""){
   			// 判断是否填写
-  			$res = new Res();
-  			$res->$code = 1;
-  			$res->$message = "用户名和密码为空";
-  			echo $res;
+  			$data['code'] = 1;
+  			$data['message'] = "用户名和密码为空";
+  			echo json_encode($data);
 			}
 			if($password == $repeatPassword){
 				// 判断输入密码和重复密码是否一样
 				$dbusername = null;
-				$result = mysqli_query("select * from user where username='{$username}'");
+				$result = mysqli_query($link, "select * from user where username='{$username}'");
 				while ($row=mysqli_fetch_array($result)){
 					$dbusername = $row["username"];
 				}
 				if(!is_null($dbusername)){
 					// 用户已存在
-					$res = new Res();
-					$res->$code = 2;
-					$res->$message = "该用户名已存在";
-					echo $res;
+					$data['code'] = 2;
+					$data['message'] = "该用户名已存在";
+					echo json_encode($data);
+				} else {
+					// 注册成功
+					$data['code'] = 0;
+					$data['message'] = "注册成功";
+					echo json_encode($data);
+					mysqli_query($link, "insert into user (username,password) values ('${username}', '${password}')") or die("存入数据库失败".mysqli_error());
+					mysqli_close($link);
 				}
-				// 注册成功
-				$res = new Res;
-				$res->$code = 0;
-				$res->message = "注册成功";
-				echo $res;
-				mysqli_query("insert into user (username,password) values ('${username}', '${password}')") or die("存入数据库失败".mysqli_error());
-				mysqli_close($link);
+			} else {
+				$data['code'] = 3;
+				$data['message'] = "输入密码和重复密码不一致";
+				echo json_encode($data);
 			}
-			$res = new Res();
-			$res->$code = 3;
-			$res->$message = "输入密码和重复密码不一致";
-			echo $res;
 		}
 	}
 ?>
